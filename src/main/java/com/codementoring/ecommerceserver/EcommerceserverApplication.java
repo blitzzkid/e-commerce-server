4d46da4aa74730c4a90c7d4d56c35b764a82a6aa
mtan014 <mtan014@e.ntu.edu.sg>
2023-02-06 00:51:20 +0800

    Intial commit

---
+++ src/main/java/com/codementoring/ecommerceserver/EcommerceserverApplication.java
@@ -0,0 +1,13 @@
+package com.codementoring.ecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codementoring/ecommerceserver/EcommerceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codementoring.ecommerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
