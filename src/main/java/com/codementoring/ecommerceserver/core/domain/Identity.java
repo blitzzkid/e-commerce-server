30bf0586a2fabbada8e4989f58baa73c94e748f6
melvin <tan.melvin@outlook.sg>
2023-03-19 16:23:31 +0800

    Add Lombok library, Identity and ProductCategory domain

---
+++ src/main/java/com/codementoring/ecommerceserver/core/domain/Identity.java
@@ -0,0 +1,12 @@
+package com.codementoring.ecommerceserver.core.domain;
+
+import lombok.Value;
+
+@Value
+public class Identity {
+    Long number;
+
+    public static Identity none() {
+        return new Identity(Long.MIN_VALUE);
+    }
+}
---
+++ src/main/java/com/codementoring/ecommerceserver/core/domain/ProductCategory.java
@@ -0,0 +1,9 @@
+package com.codementoring.ecommerceserver.core.domain;
+
+import lombok.Value;
+
+@Value
+public class ProductCategory {
+    Identity id;
+    String Name;
+}
