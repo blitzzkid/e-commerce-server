2ecda1e4e5e8cf789934849d899c60badf95612a
melvin <tan.melvin@outlook.sg>
2023-04-30 19:37:41 +0800

    Move location of Application class

--- src/main/java/com/ecommerceserver/presenter/Application.java
+++ src/main/java/com/ecommerceserver/presenter/Application.java
@@ -1,4 +1,4 @@
-package com.ecommerceserver.presenter.restapi;
+package com.ecommerceserver.presenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
